40c82b7138191e83f91bb5e9c115ecc6f4a2c9b1
karsan99 <karenjsanchez@galileo.edu>
2023-09-22 09:21:12 -0600

    Create lab8-script-evaluacion.sh

---
+++ lab8-script-evaluacion.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Este es un archivo para autoevaluar el avance y para corregir la entrega del laboratorio.
+
+clear
+echo "Laboratorio 8 - Revision"
+
+echo "Ud debe correr este script y mostrar el resultado a su Asistente de Laboratorio."
+echo
+
+echo "Ejercicio No.1"
+echo "Revisión de script con menú"
+echo "---------------------------------------"
+sudo bash script.sh
+echo "---------------------------------------"
